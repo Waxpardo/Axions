d37289ccd9063e89709ee5376a5d9bef5b1605b2
Camille Wydemans <cwydeman@stbc-i1.nikhef.nl>
2026-05-28 13:51:22 +0200

    Setting up setupEnv.sh file

---
+++ setupEnv.sh
@@ -0,0 +1,6 @@
+#/bin/bash
+source /cvmfs/alice.cern.ch/etc/login.sh 
+export ALICE_WORK_DIR=/cvmfs/alice.cern.ch/
+source /cvmfs/sft.cern.ch/lcg/views/LCG_105/x86_64-el9-gcc12-opt/setup.sh
+which mg5_aMC
+which pythia8-config
